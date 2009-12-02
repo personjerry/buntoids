bcaa6a8b2e483f2122ff1aa856f739ffd4da6ac7
Jerry Liu <personjerry@gmail.com>
2009-12-02 17:20:54 -0500

    Revert "initial import."
    This reverts commit cf2da5548fa2459cf0c3c1f4e145b0d29b6f15f9.

--- source/file.h
+++
@@ -1,8 +0,0 @@
-#ifndef _FILE_
-#define _FILE_
-
-void savegame();
-void loadgame();
-
-#endif
-
--- source/input.h
+++
@@ -1,9 +0,0 @@
-#ifndef _INPUT_
-#define _INPUT_
-
-extern bool KEY_E_DOWN;
-
-void input_update();
-
-#endif
-
