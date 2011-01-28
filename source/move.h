5caa16eb4b8bd811ef52ce8bc470fbe5036816fe
Rolando Pereira <finalyugi@sapo.pt>
2011-01-28 21:40:47 +0000

    Indent correction for move.h

--- source/move.h
+++ source/move.h
@@ -7,13 +7,14 @@
 #include <cstdlib>
 
 struct Move {
-        int id;
+	int id;
 	std::string name;
-        int type;
-        int power;
-        int acc;
-        int pp;
+	int type;
+	int power;
+	int acc;
+	int pp;
 };
+
 extern std::vector<Move> moves;
 extern unsigned int movecount;
 extern void move_init();
